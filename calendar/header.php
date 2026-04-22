f9103d954d507215cc9ac2e639e830e7b576ea37
CSU-Ryan <enderbro@colostate.edu>
2026-04-21 20:06:06 -0600

    calendar header

---
+++ calendar/header.php
@@ -0,0 +1,8 @@
+<div id="calendar-header">
+    <a id="prior-month" class="calendar-button">Last Month Year</a>
+    <a id="next-month" class="calendar-button">Next Month Year</a>
+
+    <h2 id="month-header">Month Year</h2>
+
+    <a id="new-event" class="calendar-button">Create New Event</a>
+</div>
