6b9f59337c622fbf0fa8e6c2373b5828457d76f4
CSU-Ryan <enderbro@colostate.edu>
2026-04-21 20:40:14 -0600

    Makes id formatting consistent

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -1,17 +1,17 @@
 <section id='calendar'>
     <table>
         <tr id='week-days'>
-            <th id='Sunday'>Sunday</th>
-            <th id='Monday'>Monday</th>
-            <th id='Tuesday'>Tuesday</th>
-            <th id='Wednesday'>Wednesday</th>
-            <th id='Thursday'>Thursday</th>
-            <th id='Friday'>Friday</th>
-            <th id='Saturday'>Saturday</th>
+            <th id='sunday'>Sunday</th>
+            <th id='monday'>Monday</th>
+            <th id='tuesday'>Tuesday</th>
+            <th id='wednesday'>Wednesday</th>
+            <th id='thursday'>Thursday</th>
+            <th id='friday'>Friday</th>
+            <th id='saturday'>Saturday</th>
         </tr>
         <?php
             for ($i = 0; $i < 5; $i++) {
-                echo "<tr id='row{$i}'>";
+                echo "<tr id='row-{$i}'>";
                 for ($j = 0; $j < 7; $j++) {
                     echo "<td id='calendar-{$i}-{$j}'></td>";
                 };
