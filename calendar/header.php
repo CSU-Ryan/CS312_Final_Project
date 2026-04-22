0e602b6c4f04f1f2a43eda61b52375d75e08b211
CSU-Ryan <enderbro@colostate.edu>
2026-04-21 20:39:17 -0600

    Switches single and double quotes

--- calendar/header.php
+++ calendar/header.php
@@ -1,8 +1,8 @@
-<div id="calendar-header">
-    <a id="prior-month" class="calendar-button">Last Month Year</a>
-    <a id="next-month" class="calendar-button">Next Month Year</a>
+<div id='calendar-header'>
+    <a id='prior-month' class='calendar-button'>Last Month Year</a>
+    <a id='next-month' class='calendar-button'>Next Month Year</a>
 
-    <h2 id="month-header">Month Year</h2>
+    <h2 id='month-header'>Month Year</h2>
 
-    <a id="new-event" class="calendar-button">Create New Event</a>
+    <a id='new-event' class='calendar-button'>Create New Event</a>
 </div>
