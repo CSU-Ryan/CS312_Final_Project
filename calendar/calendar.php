31da51e6360b91697ad7ff29814d56c74cd9b786
CSU-Ryan <enderbro@colostate.edu>
2026-04-23 16:29:42 -0600

    Puts divs into each calendar cell

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -1,19 +1,19 @@
 <section id='calendar'>
     <table>
         <tr id='week-days'>
-            <th>Sunday</th>
-            <th>Monday</th>
-            <th>Tuesday</th>
-            <th>Wednesday</th>
-            <th>Thursday</th>
-            <th>Friday</th>
-            <th>Saturday</th>
+            <th><div id='sun' class='calendar-header'>Sunday</div></th>
+            <th><div id='mon' class='calendar-header'>Monday</div></th>
+            <th><div id='tues' class='calendar-header'>Tuesday</div></th>
+            <th><div id='wed' class='calendar-header'>Wednesday</div></th>
+            <th><div id='thurs' class='calendar-header'>Thursday</div></th>
+            <th><div id='fri' class='calendar-header'>Friday</div></th>
+            <th><div id='sat' class='calendar-header'>Saturday</div></th>
         </tr>
         <?php
             for ($i = 0; $i < 5; $i++) {
                 echo "<tr id='row-{$i}'>";
                 for ($j = 0; $j < 7; $j++) {
-                    echo "<td id='calendar-{$i}-{$j}'></td>";
+                    echo "<td><div id='calendar-{$i}-{$j}' class='calendar-cell'></div></td>";
                 };
                 echo "</tr>";
             };
