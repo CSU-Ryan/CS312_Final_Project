c73b20f2481dff2a17c994a155d29f9ddf7fbcfc
CSU-Ryan <enderbro@colostate.edu>
2026-04-23 13:46:59 -0600

    metadata for calendar

---
+++ calendar/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Calendar View</title>
+    <meta name="author" content="Ryan Grimm">
+    <meta name="description" content="Page for viewing personal monthly calendar.">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <?php
+        include('header.php');
+        include('calendar.php');
+    ?>
+</body>
+
+</html>
