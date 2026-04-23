5f35f2d0944b93ce003799937ed172bafda745dc
CSU-Ryan <enderbro@colostate.edu>
2026-04-23 14:05:52 -0600

    Fills in sign in and sign up pages

---
+++ account/sign-in.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Sign-In Page</title>
+    <meta name="author" content="Ryan Grimm">
+    <meta name="description" content="Page for logging in to your calendar account.">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <section id="sign-in">
+        <form action="#" method="post">
+            <h2>Sign In</h2>
+
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Username" required>
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Password" required>
+
+            <button type="submit">Sign in</button>
+        </form>
+
+        <div>
+            Don't have an account? <a href="sign-up.php">Sign up here.</a>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ account/sign-up.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Sign-Up Page</title>
+    <meta name="author" content="Ryan Grimm">
+    <meta name="description" content="Page for creating a calendar account.">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <section id="sign-up">
+        <form action="#" method="post">
+            <h2>Sign Up</h2>
+
+            <label for="email">Email address</label>
+            <input type="email" name="email" id="email" placeholder="Email address" required>
+
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="Username" required>
+
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Password" required>
+
+            <button type="submit">Sign Up</button>
+        </form>
+
+        <div>
+            Already have an account? <a href="sign-in.php">Sign in here.</a>
+        </div>
+    </section>
+</body>
+
+</html>
