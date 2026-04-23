c650cead0217b342730b6eb6fcc39eed84a0e952
CSU-Ryan <enderbro@colostate.edu>
2026-04-23 14:11:44 -0600

    Writes basic stylesheet for account

--- account/sign-in.php
+++ account/sign-in.php
@@ -8,10 +8,11 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="account.css">
 </head>
 
 <body>
-    <section id="sign-in">
+    <section id="sign-in" class="account-form">
         <form action="#" method="post">
             <h2>Sign In</h2>
 
--- account/sign-up.php
+++ account/sign-up.php
@@ -8,10 +8,11 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="account.css">
 </head>
 
 <body>
-    <section id="sign-up">
+    <section id="sign-up" class="account-form">
         <form action="#" method="post">
             <h2>Sign Up</h2>
 
