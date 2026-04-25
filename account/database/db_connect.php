12f490c2ab48026389545852c96d9782fff7b432
CSU-Ryan <enderbro@colostate.edu>
2026-04-25 15:20:39 -0600

    Writes default db connection

---
+++ account/database/db_connect.php
@@ -0,0 +1,10 @@
+<?php
+$server = '';
+$database = '';
+$username = '';
+$password = '';
+
+$conn = new mysqli($server, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
