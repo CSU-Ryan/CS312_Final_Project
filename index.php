ba6f269cbe3777cb6d6473c6a32a0e357a1ef1db
CSU-Ryan <enderbro@colostate.edu>
2026-04-25 15:23:09 -0600

    Adds code to forward user from index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Directory: Forwards user to appropriate page based on variables.
+
+if (isset($_SESSION['id'])) {
+    header('Location: /calendar/index.php');
+    exit();
+} else {
+    header('Location: /account/sign-in.php');
+    exit();
+}
