572f7558fe55c6a2defd872f828305759fff30d8
CSU-Ryan <enderbro@colostate.edu>
2026-04-25 15:47:13 -0600

    Writes in database info

--- account/database/db_connect.php
+++ account/database/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$server = '';
-$database = '';
-$username = '';
-$password = '';
+$server = 'helmi.cs.colostate.edu';
+$database = 'enderbro';
+$username = 'enderbro';
+$password = 'seltzer-estrogen-quickly';
 
 $conn = new mysqli($server, $username, $password, $database);
 if ($conn->connect_error) {
