6d8b19bcab392d64112f212252ef27389dd5216c
CSU-Ryan <enderbro@colostate.edu>
2026-04-26 12:00:10 -0600

    Adds session start

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 // Directory: Forwards user to appropriate page based on variables.
 
+session_start();
 if (isset($_SESSION['id'])) {
     header('Location: ./calendar/index.php');
 } else {
