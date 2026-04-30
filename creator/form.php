40872b462e6ac315ebacfd7f074ea5d3ba0c8ed7
CSU-Ryan <enderbro@colostate.edu>
2026-04-30 11:35:23 -0600

    Better form formatting

--- creator/form.php
+++ creator/form.php
@@ -1,12 +1,12 @@
 <form action=".." method="POST">
     <div><label for="name">
         Event Name:
-        <input type="text" name="name" placeholder="..." required>
+        <input type="text" name="name" required>
     </label></div>
 
     <div><label for="date">
         Date:
-        <input type="date" name="date" placeholder="_/_/_" required>
+        <input type="date" name="date" required>
     </label></div>
 
     <div id="event-time">
@@ -14,22 +14,22 @@
         <br>
         <label for="start-time">
             From:
-            <input type="time" name="start-time" placeholder="__:__" required>
+            <input type="time" name="start-time" step="60" required>
         </label>
         <label for="end-time">
             To:
-            <input type="time" name="end-time" placeholder="__:__" required>
+            <input type="time" name="end-time" step="60" required>
         </label>
     </div>
 
     <div><label for="location">
         Location (Optional):
-        <input type="text" name="location" placeholder="...">
+        <input type="text" name="location">
     </label></div>
 
     <div><label for="description">
         Description (Optional):
-        <textarea name="description" placeholder="..."></textarea>
+        <textarea name="description"></textarea>
     </label></div>
 
     <div><button type="submit">Create New Event</button></div>
