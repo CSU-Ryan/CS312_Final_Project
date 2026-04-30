30a75099268f283d830c8d2751df55f4a5300960
CSU-Ryan <enderbro@colostate.edu>
2026-04-29 22:14:09 -0600

    Initializes buttons for calendar switching

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,5 +1,7 @@
 import { Temporal } from 'https://cdn.jsdelivr.net/npm/@js-temporal/polyfill/+esm';
 
+let month = Temporal.PlainYearMonth();
+
 function initializeDate() {
     let date_string = $(this).attr("cell-date");
 
@@ -7,14 +9,38 @@ function initializeDate() {
     if (date_string) {
         let date = Temporal.PlainDate.from(date_string);
         $(this).find(".day-label").text(date.day);
+
+        if (!month) month = date.toPlainYearMonth();
     } else {
         $(this).addClass("invalid-cell");
     }
+}
+
+function initializeButtonText() {
+    $("#prior-month").text(
+        month.add({months: -1})
+            .toLocaleString("en-US", { dateStyle: "full" })
+    );
+
+    $("#next-month").text(
+        month.add({months: 1})
+            .toLocaleString("en-US", { dateStyle: "full" })
+    );
 }
 
 
 $(document).ready(function () {
 
     $(".calendar-cell").each(initializeDate);
+
+    initializeButtonText();
+
+    $("#prior-month").click(function () {
+        window.location.href = "#?d=" + month.add({months: -1});
+    })
+
+    $("#next-month").click(function () {
+        window.location.href = "#?d=" + month.add({months: 1});
+    })
 
 })
