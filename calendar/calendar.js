3ce265efa083c39c3297c9d93b560499e9a00c78
CSU-Ryan <enderbro@colostate.edu>
2026-04-29 21:41:59 -0600

    Moves log

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -3,8 +3,8 @@ import { Temporal } from 'https://cdn.jsdelivr.net/npm/@js-temporal/polyfill/+es
 function initializeDate() {
     let date_string = $(this).attr("cell-date");
 
+    console.log("Given date string: " + date_string);
     if (date_string) {
-        console.log("Given date string: " + date_string);
         let date = Temporal.PlainDate.from(date_string);
         $(this).find(".day-label").text(date.day);
     } else {
