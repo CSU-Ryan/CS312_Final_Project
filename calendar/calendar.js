04b499160acd89ce296fdc97a3f9e6d9ddb8a1d1
CSU-Ryan <enderbro@colostate.edu>
2026-04-29 21:33:35 -0600

    Replaces import with other string

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,10 +1,11 @@
+import { Temporal } from 'https://cdn.jsdelivr.net/npm/@js-temporal/polyfill/+esm';
 
 function initializeDate() {
     let date_string = $(this).attr("cell-date");
 
     if (date_string !== '') {
-        let date = new Date(date_string);
-        $(this).find(".day-label").text(date.getDate());
+        let date = Temporal.PlainDate.from(date_string);
+        $(this).find(".day-label").text(date.day);
     } else {
         $(this).addClass("invalid-cell");
     }
