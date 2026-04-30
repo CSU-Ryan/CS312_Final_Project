e85309c9babc9b6247fffad12b0895630d46f789
CSU-Ryan <enderbro@colostate.edu>
2026-04-30 12:17:05 -0600

    Writes sql event creator

--- database/db_create_event.php
+++ database/db_create_event.php
@@ -1,2 +1,28 @@
 <?php
+session_start();
 
+if (!isset($_SESSION['id'])) return;
+$id = $_SESSION['id'];
+
+$name = $_POST['name'];
+$date = $_POST['date'];
+$start = $_POST['start-time'];
+$end = $_POST['end-time'];
+$location = $_POST['location'] ?? null;
+$description = $_POST['description'] ?? null;
+
+include 'db_connect.php';
+
+/** @noinspection PhpUndefinedVariableInspection */
+$new_entry = $conn->prepare("INSERT INTO events (id, name, date, start, end, location, description) 
+                                   VALUES (?, ?, ?, ?, ?, ?, ?)");
+$new_entry->bind_param("issssss",
+    $id, $name, $date, $start, $end, $location, $description);
+
+if ($new_entry->execute()) {
+    header('Location: ..');
+} else {
+    $message = 'Error creating new event: ' . $conn->error;
+};
+$new_entry->close();
+$conn->close();
