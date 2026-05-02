1a0741317608790c896c0bf199f3a8b375b6e9b8
CSU-Ryan <enderbro@colostate.edu>
2026-05-02 11:53:30 -0600

    Writes index for event

---
+++ event/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Event View</title>
+    <meta name="author" content="Ryan Grimm">
+    <meta name="description" content="Detail view of event from personal calendar.">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>-->
+</head>
+
+<body>
+
+<?php
+include 'event.php';
+?>
+
+</body>
+</html>
