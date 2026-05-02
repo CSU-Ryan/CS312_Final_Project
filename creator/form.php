bc49a6a06bbfc7abda1bbd1af73f722ec6c03187
CSU-Ryan <enderbro@colostate.edu>
2026-05-01 21:10:12 -0600

    Adds styling to event form

--- creator/form.php
+++ creator/form.php
@@ -2,41 +2,43 @@
     include 'parse_form.php';
 ?>
 
-<form method="POST">
-    <div><label for="name">
-        Event Name:
-        <input type="text" name="name" required>
-    </label></div>
+<section id="event-creator">
+    <form method="POST">
+        <div class="field"><label for="name">
+            Event Name:
+            <input type="text" name="name" required>
+        </label></div>
 
-    <div><label for="date">
-        Date:
-        <input type="date" name="date" required>
-    </label></div>
+        <div class="field"><label for="date">
+            Date:
+            <input type="date" name="date" required>
+        </label></div>
 
-    <div id="event-time">
-        Event Time:
-        <br>
-        <label for="start-time">
-            From:
-            <input type="time" name="start-time" step="60" required>
-        </label>
-        <label for="end-time">
-            To:
-            <input type="time" name="end-time" step="60" required>
-        </label>
-    </div>
+        <div class="field" id="event-time">
+            Event Time:
+            <br>
+            <label for="start-time">
+                From:
+                <input type="time" name="start-time" step="60" required>
+            </label>
+            <label for="end-time">
+                To:
+                <input type="time" name="end-time" step="60" required>
+            </label>
+        </div>
 
-    <div><label for="location">
-        Location (Optional):
-        <input type="text" name="location">
-    </label></div>
+        <div class="field"><label for="location">
+            Location (Optional):
+            <input type="text" name="location">
+        </label></div>
 
-    <div><label for="description">
-        Description (Optional):
-        <textarea name="description"></textarea>
-    </label></div>
+        <div class="field"><label for="description">
+            Description (Optional):
+            <textarea name="description"></textarea>
+        </label></div>
 
-    <div id="Validation"><?php echo $message; ?></div>
+        <div id="Validation"><?php echo $message; ?></div>
 
-    <div><button type="submit">Create New Event</button></div>
-</form>
+        <div><button type="submit">Create New Event</button></div>
+    </form>
+</section>
--- creator/index.php
+++ creator/index.php
@@ -1,3 +1,26 @@
 <?php
     session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Event Creator</title>
+    <meta name="author" content="Ryan Grimm">
+    <meta name="description" content="Event creator for personal calendar.">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+<?php
     include 'form.php';
+?>
+
+</body>
+</html>
