52112deda735f0d8da6fcf5cbf7694b2d660884e
CSU-Ryan <enderbro@colostate.edu>
2026-05-02 11:07:01 -0600

    Adds styling to event.php

--- event/event.php
+++ event/event.php
@@ -13,20 +13,20 @@ include '../database/db_retrieve_event.php';
     <h2 id="event-name"><?php echo $name; ?></h2>
 
     <div id="event-details">
-        <div id="event-date">
-            Date: <span><?php echo $date; ?></span>
+        <div id="event-date" class="event-detail">
+            <em>Date:</em> <span><?php echo $date; ?></span>
         </div>
 
-        <div id="event-time">
-            Time: <span><?php echo $start . ' - ' . $end; ?></span>
+        <div id="event-time" class="event-detail">
+            <em>Time:</em> <span><?php echo $start . ' - ' . $end; ?></span>
         </div>
 
-        <div id="event-location">
-            Location: <span><?php echo $location ?? ''; ?></span>
+        <div id="event-location" class="event-detail">
+            <em>Location:</em> <span><?php echo $location ?? ''; ?></span>
         </div>
 
-        <div id="event-description">
-            Description: <p><?php echo $description; ?></p>
+        <div id="event-description" class="event-detail">
+            <em>Description:</em> <p><?php echo $description; ?></p>
         </div>
     </div>
 </section>
