0f2c430b53ef6cbe851fcfaaedcca859d72b5912
CSU-Ryan <enderbro@colostate.edu>
2026-05-03 15:31:39 -0600

    Corrects data references for filling content of event div

--- calendar/calendar_events.js
+++ calendar/calendar_events.js
@@ -4,7 +4,7 @@ function make_event(event_data) {
     event.id = "event-" + event_data.event_id;
     event.className = "calendar-event";
 
-    event.innerHTML = `<em>${event.name}</em> ${event.start} - ${event.end}`;
+    event.innerHTML = `<em>${event_data.name}</em> ${event_data.start} - ${event_data.end}`;
 
     return event;
 }
