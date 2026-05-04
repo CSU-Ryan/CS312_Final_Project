5ccd53a79299ab361d346b10f2c08d0e9b61007c
CSU-Ryan <enderbro@colostate.edu>
2026-05-03 21:45:10 -0600

    Temporarily removes account link

--- statics/navigation.php
+++ statics/navigation.php
@@ -2,6 +2,6 @@
     <ul>
         <li><a href="../calendar/">Calendar</a></li>
         <li><a href="../creator/">Create New Event</a></li>
-        <li><a href="../account/">Account</a></li>
+<!--        <li><a href="../account/">Account</a></li>-->
     </ul>
 </nav>
