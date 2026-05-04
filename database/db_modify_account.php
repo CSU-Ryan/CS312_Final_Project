740bf4a81d68b5be8ee235d725b7cf2fc740c4e4
CSU-Ryan <enderbro@colostate.edu>
2026-05-04 13:22:43 -0600

    Writes database file for modifying account

---
+++ database/db_modify_account.php
@@ -0,0 +1,59 @@
+<?php
+
+function change_value($parameter, $new_value, $user_id, $conn) {
+    $modify = $conn->prepare("UPDATE users SET parameter = ? WHERE id = ?");
+    $modify->bind_param('si', $new_value, $user_id);
+    $modify->execute();
+
+    if ($modify->affected_rows == 0) {
+        $message = 'Error: No user with your ID.';
+    } else {
+        $message = "$parameter changed successfully.";
+    }
+    $modify->close();
+
+    return $message;
+}
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+include 'db_connect.php';
+
+if (!isset($_SESSION['user_id'])) {
+    $message = 'Error: Not logged in.';
+} else {
+    $user_id = $_SESSION['user_id'];
+
+    if ($_POST['change_email']) {
+        $message = change_value('email', $_POST['email'], $user_id, $conn);
+    }
+
+    if ($_POST['change_username']) {
+        $message = change_value('name', $_POST['username'], $user_id, $conn);
+    }
+
+    if ($_POST['change_password']) {
+        $new_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $message = change_value('password', $new_password, $user_id, $conn);
+    }
+
+    if ($_POST['delete_account']) {
+        $deleteAccount = $conn->prepare("DELETE FROM users WHERE id = ?");
+        $deleteAccount->bind_param('i', $user_id);
+        $deleteAccount->execute();
+
+        if ($deleteAccount->affected_rows == 0) {
+            $message = 'Error: No user with your ID.';
+        } else {
+            $message = 'Account deleted successfully.';
+
+            $deleteEvents = $conn->prepare("DELETE FROM events WHERE user_id = ?");
+            $deleteEvents->bind_param('i', $user_id);
+            $deleteEvents->execute();
+            $deleteEvents->close();
+        }
+        $deleteAccount->close();
+    }
+}
