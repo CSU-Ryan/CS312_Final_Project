14ddd85825f1b0c51c09a46f3023c648eac5d531
CSU-Ryan <enderbro@colostate.edu>
2026-05-04 21:01:50 -0600

    Adds logout button

--- statics/navigation.php
+++ statics/navigation.php
@@ -3,5 +3,6 @@
         <li><a href="../calendar/">Calendar</a></li>
         <li><a href="../creator/">Create New Event</a></li>
         <li><a href="../account/account.php">Account</a></li>
+        <li><a href="../account/sign-out.php">Logout</a></li>
     </ul>
 </nav>
