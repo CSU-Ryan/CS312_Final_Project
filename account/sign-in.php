20cdd9df6d3a4c8b6d2cbb259178a0fdb0b68d3a
CSU-Ryan <enderbro@colostate.edu>
2026-05-04 20:59:12 -0600

    Corrects header for successful sign in

--- account/sign-in.php
+++ account/sign-in.php
@@ -1,13 +1,13 @@
 <?php
+$message = '';
+include "../database/db_connect_account.php";
+
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 if (isset($_SESSION['user_id'])) {
     header('Location: ../index.php');
 }
-
-$message = '';
-include "../database/db_connect_account.php";
 ?>
 
 <!DOCTYPE html>
